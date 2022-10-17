1bfea194a400eeeaf9d69f63bdde689cf65d8d7a
Innovin <102300150+InnovinAC@users.noreply.github.com>
2022-10-18 00:36:16 +0100

    Create code.py

---
+++ Day 1 Project: Band Name Generator/code.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n").upper()
+pet = input("What's the name of your pet?\n").upper()
+print(f"Your Band name could be {city} {pet}!")
